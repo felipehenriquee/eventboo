496234566315a3fe57e661b14bf38eb20cb68c6a
felipehenriquee <79702834+felipehenriquee@users.noreply.github.com>
2021-09-01 17:37:26 -0300

    Add files via upload

--- canvas-entrance.js
+++ canvas-entrance.js
@@ -8,26 +8,26 @@ var proportion = screen.width / screen.height;
 window.onload = function () {
     console.log('load canvas');
     canvas = document.getElementById("myCanvas");
-    // imgBg = document.getElementById("bgImage");
+    imgBg = document.getElementById("bgImage");
     ctx = canvas.getContext("2d");
 
     var date = new Date();
     console.log(date.getHours())
     
-    if (date.getHours()>=18 || date.getHours()+8<=4){
+    if (date.getHours()>=18 || date.getHours()<=4){
         img.src = './3DImages/ExternaNoturnaR01V02.jpg';
-        // imgBg.style.backgroundImage = "url('./3DImages/ExternaNoturnaR01V02.jpg')";
+        imgBg.style.backgroundImage = "url('./3DImages/ExternaNoturnaR01V02.jpg')";
        
     }
     else{
         img.src = './3DImages/Externa_Diurna_R01_v02.jpg';
-        // imgBg.style.backgroundImage = "url('./3DImages/Externa_Diurna_R01_v02.jpg')";
+        imgBg.style.backgroundImage = "url('./3DImages/Externa_Diurna_R01_v02.jpg')";
 
     }
 
     
     img.onload = function(){
-//     document.getElementById('comeceAqui').focus();
+    // document.getElementById('comeceAqui').focus();
 
         console.log('loaded');
         ctx.drawImage(img, 0, 0, 1920, 1080);
